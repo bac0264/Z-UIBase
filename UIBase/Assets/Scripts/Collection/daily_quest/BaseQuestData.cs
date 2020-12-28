69514ca711d96ac61acbba6426347cf0b9bd90b2
bac <bacnt@zitga.com.vn>
2020-12-28 10:30:18 +0700

    add quest

---
+++ UIBase/Assets/Scripts/Collection/daily_quest/BaseQuestData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BaseQuestData
+{
+    [SerializeField] public int id;
+    [SerializeField] public int type;
+    [SerializeField] public int idTarget;
+    [SerializeField] public long required;
+    [SerializeField] public Reward[] rewards;
+    [SerializeField] public string iconName;
+    [SerializeField] private string description;
+    
+
+    public string Description
+    {
+        get { return GetDescription(); }
+    }
+
+    public bool CanGoto()
+    {
+        return false; //QuestAchievementHelper.CanGoToQuest((QuestType) type);
+    }
+
+    public string GetDescription()
+    {
+        return ""; // QuestAchievementHelper.GetDescription(type, description, required, idTarget);
+    }
+}
--- UIBase/Assets/Scripts/Collection/daily_quest/DailyQuestCollection.cs
+++ UIBase/Assets/Scripts/Collection/daily_quest/DailyQuestCollection.cs
@@ -2,7 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class DailyQuestCollection : ScriptableObject
+{
+    public DailyQuestData[] dataGroups;
+}
+
+[System.Serializable]
+public class DailyQuestData : BaseQuestData
 {
     
 }
